05273bd4bab8e3a612d49b9d82e4be6a53f96d20
IceCao <riskhiakbar@gmail.com>
2025-06-26 09:42:48 +0700

    update public

--- public/index.php
+++ public/index.php
@@ -2,29 +2,14 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-/*
- *---------------------------------------------------------------
- * SYSTEM FOLDER NAME AND APPLICATION FOLDER NAME
- *---------------------------------------------------------------
- */
 $system_path = '../system';
 $application_folder = '../application';
 
-/*
- *---------------------------------------------------------------
- * Resolve the system path for increased reliability
- *---------------------------------------------------------------
- */
 $system_path = realpath($system_path) ?: $system_path;
 $system_path = rtrim($system_path, '/').'/';
 
 define('BASEPATH', str_replace("\\", "/", $system_path));
 
-/*
- *---------------------------------------------------------------
- * Set the application directory
- *---------------------------------------------------------------
- */
 if (is_dir($application_folder)) {
     define('APPPATH', $application_folder.'/');
 } else {
@@ -34,9 +19,4 @@ if (is_dir($application_folder)) {
     define('APPPATH', BASEPATH.$application_folder.'/');
 }
 
-/*
- *---------------------------------------------------------------
- * Load the framework bootstrap file
- *---------------------------------------------------------------
- */
 require_once BASEPATH.'core/CodeIgniter.php';
