3ec72b24ba07c3662322055487082611355325c6
IceCao <riskhiakbar@gmail.com>
2025-06-26 09:06:48 +0700

    Update config

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'http://localhost/toko/';
-$config['base_url'] = 'https://toko-maju-jaya.netlify.app';
+$config['base_url'] = 'https://toko-maju-jaya.up.railway.app';
 
 /*
 |--------------------------------------------------------------------------
